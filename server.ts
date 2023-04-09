8dbef2c3fdc7914f6e2196365d44678eb3b5f76c
Кирилл Дудников <kddudnikov@edu.hse.ru>
2023-04-10 01:15:58 +0300

    Start custom server try 3

--- server.ts
+++ server.ts
@@ -2,20 +2,28 @@ const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
-const port = parseInt(process.env.PORT || '3000', 10)
+const port = 3000
+const hostname = '23.95.246.136'
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
-
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
+    try {
+      const parsedUrl = parse(req.url, true)
+      handle(req, res, parsedUrl)
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
+    }
+  })
+    .once('error', (err) => {
+      console.error(err)
+      process.exit(1)
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`)
+    })
 })
