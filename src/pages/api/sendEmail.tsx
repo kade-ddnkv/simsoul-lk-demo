5a4253a96561231fa830b7358fc27a379ee9d272
Кирилл Дудников <kddudnikov@edu.hse.ru>
2023-04-18 21:43:11 +0300

    Feedback now send email

---
+++ src/pages/api/sendEmail.tsx
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+const nodemailer = require('nodemailer');
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { subject, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD,
+      }
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_ADDRESS,
+      to: process.env.EMAIL_ADDRESS,
+      subject,
+      text: message
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+      } else {
+        console.log('Email sent: ' + info.response);
+        res.status(200).json({ message: 'Email sent successfully' });
+      }
+    });
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
--- src/pages/feedback.tsx
+++ src/pages/feedback.tsx
@@ -6,21 +6,40 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import { useUser } from '@/auth/useUser'
 import Header from '@/components/header';
+import axios from 'axios';
+import { GetServerSidePropsContext } from 'next'
+import { serverSideAuthCheck } from '@/auth/serverSideAuthCheck'
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  return serverSideAuthCheck(context)
+}
 
 export default function LeaveFeedback() {
   const [feedbackText, setFeedbackText] = useState('')
 
+  const postFeedbackToEmail = async () => {
+    try {
+      const response = await axios.post('/api/sendEmail', {
+        subject: 'Feedback on simsoul-lk-demo',
+        message: feedbackText,
+      });
+      console.log(response.data.message);
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  }
+
   return (
     <>
       <Container maxWidth='xl' sx={{ mt: 2 }}>
-        <Header mainText='simsoul demo' noLine noRight useHomeButton={true} isBold={false} />
+        <Header mainText='simsoul demo' noLine useHomeButton={true} isBold={false} />
       </Container>
       <Container maxWidth='lg' sx={{ height: '80vh', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-        <Stack component={Container} spacing={2} sx={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'  }}>
+        <Stack component={Container} spacing={2} sx={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
           <Container maxWidth='xs' sx={{ display: 'flex', alignItems: 'center' }}>
             <HeaderText sx={{ ml: 'auto', mr: 'auto' }}>Your feedback on demo</HeaderText>
           </Container>
-          <Container maxWidth={false} style={{maxWidth: '750px'}} sx={{ display: 'flex', alignItems: 'center' }}>
+          <Container maxWidth={false} style={{ maxWidth: '750px' }} sx={{ display: 'flex', alignItems: 'center' }}>
             <StyledTextField
               sx={{ width: '100%' }}
               minRows={10}
@@ -28,10 +47,11 @@ export default function LeaveFeedback() {
               label=''
               defaultValue=''
               multiline
+              onBlur={(event) => setFeedbackText(event.target.value)}
             />
           </Container>
           <Container maxWidth='xs'>
-            <HoverBlackButton type='submit' fullWidth variant='outlined'>Send feedback</HoverBlackButton>
+            <HoverBlackButton onClick={postFeedbackToEmail} fullWidth variant='outlined'>Send feedback</HoverBlackButton>
           </Container>
         </Stack>
       </Container>
